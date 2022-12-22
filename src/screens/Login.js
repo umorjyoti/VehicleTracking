85b6f189fdf4bbd648b3035127751a411e420a32
Umorjyoti Chetia <umorjyotichetia@gmail.com>
2022-12-22 10:23:15 +0530

    login functionality setup

---
+++ src/screens/Login.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, Touchable, TouchableOpacity} from 'react-native';
+import axios from 'react-native-axios';
+
+const Login = ({navigation}) => {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const loginData = {
+    username,
+    password,
+  };
+
+  const handleLogin = () => {
+    axios
+      .post('https://staging-api.tracknerd.io/v1/auth/login', loginData)
+      .then(res => console.log(res));
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={{borderWidth: 1}}
+        onChangeText={setUserName}
+        value={username}
+      />
+      <TextInput
+        style={{borderWidth: 1}}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <TouchableOpacity
+        onPress={handleLogin}
+        style={{
+          backgroundColor: 'yellow',
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Login;
